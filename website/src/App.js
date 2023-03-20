b8994383b9a26fecd60c43f939eb59c650049b3f
Selena <selenazheng3@gmail.com>
2023-03-20 09:45:45 -0700

    website

---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import PageWrapper from './components/PageWrapper';
+
+function App() {
+  return (
+    <PageWrapper />
+  );
+}
+
+export default App;
